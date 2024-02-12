669d9557a2890a1f30b3117dcb79126830817f58
WhiteWolf <95250432+NaumenkoVanya@users.noreply.github.com>
2024-02-12 14:52:40 +0300

    add padding and background

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -10,10 +10,24 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            Button("Ivan Developer") {
+                print(type(of: self.body))
+            }
+            .frame(width: 200,height: 200)
+            .background(.red)
+            
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+                .padding()
+                .background(.red)
+                .padding()
+                .background(.blue)
+                .padding()
+                .background(.green)
+                .padding()
+                .background(.yellow)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(.mint)
