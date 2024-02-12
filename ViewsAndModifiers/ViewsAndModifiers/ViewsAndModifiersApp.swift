4bf67c2ce251b273cc3dad25e6399ebe7f761990
WhiteWolf <95250432+NaumenkoVanya@users.noreply.github.com>
2024-02-12 14:12:58 +0300

    add new project

---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Ваня Науменко on 12.02.24.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
